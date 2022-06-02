31b32192a7d14c69433f77ff5cfd0fc6879130bc
Thad Blankenship <thad@monthly.com>
2022-06-01 21:33:30 -0500

    Updated metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'A personal blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://thaddeus.vercel.app',
+  siteUrl: 'https://tblankenship.dev',
   siteRepo: 'https://github.com/tblankenship/portfolio',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
@@ -19,7 +19,7 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: 'thaddeus.vercel.app', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'tblankenship.dev', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
